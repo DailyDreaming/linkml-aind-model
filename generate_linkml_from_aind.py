b90ae95c3ca4144d173f542b22e13a540f57d70a
DailyDreaming <lblauvel@ucsc.edu>
2024-02-07 12:16:33 -0800

    Refactor and add all modules.

--- generate_linkml_from_aind.py
+++ generate_linkml_from_aind.py
@@ -9,62 +9,53 @@ from linkml_runtime.utils.schemaview import SchemaView, ElementName, Permissible
 from linkml_runtime.utils.schema_builder import SchemaBuilder
 from linkml_runtime.dumpers import yaml_dumper
 
-def classes_from_module(module_name: str):
-    module = importlib.import_module(module_name)
-    module_classes = inspect.getmembers(module,
-                                 lambda x: inspect.isclass(x) and
-                                           issubclass(x, pydantic.BaseModel))
-    return module_classes
 
-def enums_from_module(module_name: str):
-    """Get pydantic enums
+def get_all_modules(imported_modules: list, root_module_name: str):
+    try:
+        module = importlib.import_module(root_module_name)
+        imported_modules.append(module)
+        for submodule_filename in module.__loader__.get_resource_reader().contents():
+            if submodule_filename.endswith('.py') and not submodule_filename.startswith('__'):
+                get_all_modules(imported_modules, f'{root_module_name}.{submodule_filename[:-len(".py")]}')
+    except ModuleNotFoundError:
+        return imported_modules
+    return imported_modules
 
-    Parameters
-    ----------
-    module_name : str
-    """
-    module = importlib.import_module(module_name)
-    module_enums = inspect.getmembers(module,
-                                 lambda x: inspect.isclass(x) and
-                                           issubclass(x, enum.Enum))
-    return module_enums
 
-def write_yaml(module_name: str):
-    for klass in classes_from_module(module_name):
-        pass
-    for enum_obj in enums_from_module(module_name):
-        pass
+def populate_schema_builder_from_module(sb: SchemaBuilder, module: str):
+    modules = get_all_modules(imported_modules=list(), root_module_name='aind_data_schema.models')
+    for module in modules:
+        for class_name, class_object in inspect.getmembers(module, inspect.isclass):
+            if issubclass(class_object, enum.Enum):
+                try:
+                    sb.add_enum(
+                        EnumDefinition(
+                            name=class_name,
+                            permissible_values=dict(
+                                (attribute, getattr(class_object, attribute).value) for attribute in dir(class_object) if
+                                not attribute.startswith('__')),
+                        )
+                    )
+                except ValueError:
+                    pass
+            elif issubclass(class_object, pydantic.BaseModel):
+                sb.add_class(
+                    class_name,
+                    slots=class_object.model_fields,
+                    is_a=class_object.__mro__[1].__name__,
+                    class_uri=f'schema:{class_name}',
+                    description=str(class_object.__doc__).strip() or "No description"
+                )
 
-def populate_schema_builder_from_module(sb: SchemaBuilder, module: str = 'aind_data_schema.models.devices'):
-    for model_data_class in classes_from_module(module):
-        # x = model_data_class[1].__annotations__
-        # x = model_data_class[1].model_fields
-        # i = model_data_class[1].model_from_schema()
-        z = model_data_class[1].__pydantic_core_schema__
-        sb.add_class(
-            model_data_class[0],
-            slots=model_data_class[1].model_fields,
-            is_a=model_data_class[1].__mro__[1].__name__,
-            class_uri=f'schema:{model_data_class[0]}',
-            description=model_data_class[1].__doc__.strip() or "No description"
-        )
-    for model_enum in enums_from_module(module):
-        enum_class = model_enum[1]
-        sb.add_enum(
-            EnumDefinition(
-                name=model_enum[0],
-                permissible_values=dict((attribute, getattr(enum_class, attribute).value) for attribute in dir(enum_class) if not attribute.startswith('__')),
-            )
-        )
 
 def main():
     sb = SchemaBuilder()
-    devices_module = 'aind_data_schema.models.devices'
-    populate_schema_builder_from_module(sb, module=devices_module)
+    populate_schema_builder_from_module(sb, module='aind_data_schema.models')
     yml = yaml_dumper.dumps(sb.schema)
     with open('simple.yml', 'w') as f:
         f.write(yml)
     print(yml)
+
 
 if __name__ == '__main__':
     main()
