65b31528638fa78b50900ebe3dc6cf7053d6bb6b
DailyDreaming <lblauvel@ucsc.edu>
2024-02-07 15:14:33 -0800

    Add Dandi model.

--- generate_linkml_from_aind.py
+++ generate_linkml_from_aind.py
@@ -10,6 +10,18 @@ from linkml_runtime.utils.schema_builder import SchemaBuilder
 from linkml_runtime.dumpers import yaml_dumper
 
 
+# BICAN already has linkml here: https://github.com/brain-bican/models/tree/main/linkml-schema
+# Biolink also has linkml: https://github.com/biolink/biolink-model/blob/master/src/biolink_model/schema/biolink_model.yaml
+# openminds is JSON: https://github.com/openMetadataInitiative/openMINDS_core/tree/v4
+# ATOM: https://bioportal.bioontology.org/ontologies/ATOM
+# ATOM: https://github.com/SciCrunch/NIF-Ontology/blob/atlas/ttl/atom.ttl
+# ATOM: https://www.nature.com/articles/s41597-023-02389-4
+KNOWN_MODELS = {
+    'dandi': 'dandischema.models',
+    'aind': 'aind_data_schema.models'
+}
+
+
 def get_all_modules(imported_modules: list, root_module_name: str):
     try:
         module = importlib.import_module(root_module_name)
@@ -41,7 +53,7 @@ def populate_enum(sb: SchemaBuilder, enum_name: str, enum_object: enum.Enum):
 def populate_basemodel(sb: SchemaBuilder, basemodel_name: str, basemodel_object: pydantic.BaseModel):
     sb.add_class(
         basemodel_name,
-        slots=basemodel_object.model_fields,
+        slots=basemodel_object.__annotations__,
         is_a=basemodel_object.__mro__[1].__name__,
         class_uri=f'schema:{basemodel_name}',
         description=basemodel_object.__doc__.strip() if basemodel_object.__doc__ else "No description"
@@ -58,10 +70,11 @@ def populate_schema_builder_from_module(sb: SchemaBuilder, module: str):
 
 
 def main():
+    org = 'dandi'
     sb = SchemaBuilder()
-    populate_schema_builder_from_module(sb, module='aind_data_schema.models')
+    populate_schema_builder_from_module(sb, module=KNOWN_MODELS[org])
     yml = yaml_dumper.dumps(sb.schema)
-    with open('generated_linkml_models/aind.yml', 'w') as f:
+    with open(f'generated_linkml_models/{org}.yml', 'w') as f:
         f.write(yml)
     print('Success!')
 
