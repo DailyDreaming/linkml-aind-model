780f245928abadc4e6a2eab574e9ddae675d9ae5
DailyDreaming <lblauvel@ucsc.edu>
2024-02-06 16:54:13 -0800

    Add SchemaBuilder.

--- generate_linkml_from_aind.py
+++ generate_linkml_from_aind.py
@@ -1,10 +1,10 @@
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
 import importlib
-import inspect
 import pydantic
 import enum
+import inspect
+
+from linkml_runtime import SchemaView
+from linkml_runtime.utils.schema_builder import SchemaBuilder
 from linkml_runtime.dumpers import yaml_dumper
 
 def classes_from_module(module_name: str):
@@ -34,32 +34,17 @@ def write_yaml(module_name: str):
         pass
 
 def main():
-    aind_models = [v for k, v in module.__dict__.items() if not k.startswith('__')]
-
-    all_slots = list()
-    with open('simple.yml', 'w') as f:
-        f.write('classes:\n')
-        for model_name, model_module in module.__dict__.items():
-            if not model_name.startswith('__'):
-                for model_data_class in inspect.getmembers(model_module, inspect.isclass):
-                    try:
-                        print(model_data_class)
-                        print(model_data_class[1].__mro__)
-                        attributes_in_dataclass = list(model_data_class[1].__annotations__.keys())
-                        all_slots += attributes_in_dataclass
-                        print(attributes_in_dataclass)
-                        if attributes_in_dataclass:
-                            f.write(f'  {model_data_class[0]}:\n')
-                            f.write(f'    is_a: {model_data_class[1].__mro__[1].__name__}\n')
-                            f.write(f'    description: {model_data_class[1].__doc__.strip() or "No description"}\n')
-                            f.write(f'    class_uri: schema:{model_data_class[0]}\n')
-                            f.write('    attributes:\n')
-                            for a in attributes_in_dataclass:
-                                f.write(f'      {a}:\n')
-                    except AttributeError as e:
-                        print(e)
-
-    print(f'All slots found: {set(all_slots)}')
+    sb = SchemaBuilder('linkml_runtime_template.yml')
+    for model_data_class in classes_from_module('aind_data_schema.models.devices'):
+        sb.add_class(
+            model_data_class[0],
+            slots=model_data_class[1].__annotations__,
+            is_a=model_data_class[1].__mro__[1].__name__,
+            class_uri=f'schema:{model_data_class[0]}',
+            description=model_data_class[1].__doc__.strip() or "No description"
+        )
+    print(sb)
+    yaml_dumper.dumps(sb.schema)
 
 if __name__ == '__main__':
     main()
